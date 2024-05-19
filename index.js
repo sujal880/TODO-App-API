3fb9b25dfe1d28bc86ac6e0147b4a467ae9c2598
sujal880 <sujaldave880@gmail.com>
2024-05-19 17:20:29 +0530

    Sign Up Module Completed

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app=express();
 app.use(express.json());
 app.use(allroutes);
 
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:"Welcome To TODO App"
+    })
+})
+
 app.listen(3000,()=>{
     console.log(`Server is Running on 3000`);
 });
